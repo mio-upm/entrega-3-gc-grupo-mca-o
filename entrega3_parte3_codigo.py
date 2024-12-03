7dcb16eddab069af5b719b5af71d5e625d747b6e
FedeTrevi <federicot.registrazioni@gmail.com>
2024-12-03 19:13:47 +0100

    Create entrega3_parte3_codigo.py

---
+++ entrega3_parte3_codigo.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Metodos Cuantitativos Avanzados (MIO)
+
+ENTREGA 3: Asignaci´on de quir´ofanos en un hospital y generaci´on de columnas
+Modelo 3 (Gen. columnas)
+
+Grupo O
+- KIANI HANIE
+- PAEZ LEAL JAIRO EDUARDO
+- PAJARES CAMACHO JAVIER
+- TREVISAN FEDERICO
+"""
